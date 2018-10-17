c79fb0e1cdc45ac41f9844e4e74e0fce48354c3a
jjames2 <40587472+jjames2@users.noreply.github.com>
2018-10-17 01:06:19 -0500

    Added hard-coded events, week day views
    Contains 2 hard coded events and a button group to toggle between different calendar views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatButtonToggleModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card';
@@ -45,6 +45,7 @@ import { HttpClientModule } from '@angular/common/http';
       useFactory: adapterFactory
     }),
     MatButtonModule,
+    MatButtonToggleModule,
     MatCheckboxModule,
     MatToolbarModule,
     MatMenuModule,
